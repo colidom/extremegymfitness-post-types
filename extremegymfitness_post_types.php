192e745f1517353a1b3c6fb2c969a5b36baa4e14
colidom <colidom@outlook.com>
2023-06-25 21:56:35 +0100

    ¿Cuando Crear un tema y cuando un plugin?

---
+++ extremegymfitness_post_types.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: Extreme Gym Fitness - Post Types
+    Plugin URI: https://github.com/colidom/extremegymfitness-post-types
+    Description: Añade Post Types al sitio Extreme Gym Fitness.
+    Version: 1.0.0
+    Author: colidom
+    Author URI: https://github.com/colidom
+    Text Domain: extremegymfitness
+*/

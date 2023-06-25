247e279796c637ba3306469ac2b9d3a6464e0b9c
colidom <colidom@outlook.com>
2023-06-25 22:36:09 +0100

    Renombrando

--- extremegymfitness_post_types.php
+++ extremegymfitness_post_types.php
@@ -12,41 +12,41 @@
 if (!defined('ABSPATH')) die();
 
 // Registrar Custom Post Type
-function gymfitness_clases_post_type()
+function extremegymfitness_clases_post_type()
 {
 
     $labels = array(
-        'name'                  => _x('Clases', 'Post Type General Name', 'gymfitness'),
-        'singular_name'         => _x('Clase', 'Post Type Singular Name', 'gymfitness'),
-        'menu_name'             => __('Clases', 'gymfitness'),
-        'name_admin_bar'        => __('Clase', 'gymfitness'),
-        'archives'              => __('Archivo', 'gymfitness'),
-        'attributes'            => __('Atributos', 'gymfitness'),
-        'parent_item_colon'     => __('Clase Padre', 'gymfitness'),
-        'all_items'             => __('Todas Las Clases', 'gymfitness'),
-        'add_new_item'          => __('Agregar Clase', 'gymfitness'),
-        'add_new'               => __('Agregar Clase', 'gymfitness'),
-        'new_item'              => __('Nueva Clase', 'gymfitness'),
-        'edit_item'             => __('Editar Clase', 'gymfitness'),
-        'update_item'           => __('Actualizar Clase', 'gymfitness'),
-        'view_item'             => __('Ver Clase', 'gymfitness'),
-        'view_items'            => __('Ver Clases', 'gymfitness'),
-        'search_items'          => __('Buscar Clase', 'gymfitness'),
-        'not_found'             => __('No Encontrado', 'gymfitness'),
-        'not_found_in_trash'    => __('No Encontrado en Papelera', 'gymfitness'),
-        'featured_image'        => __('Imagen Destacada', 'gymfitness'),
-        'set_featured_image'    => __('Guardar Imagen destacada', 'gymfitness'),
-        'remove_featured_image' => __('Eliminar Imagen destacada', 'gymfitness'),
-        'use_featured_image'    => __('Utilizar como Imagen Destacada', 'gymfitness'),
-        'insert_into_item'      => __('Insertar en Clase', 'gymfitness'),
-        'uploaded_to_this_item' => __('Agregado en Clase', 'gymfitness'),
-        'items_list'            => __('Lista de Clases', 'gymfitness'),
-        'items_list_navigation' => __('Navegación de Clases', 'gymfitness'),
-        'filter_items_list'     => __('Filtrar Clases', 'gymfitness'),
+        'name'                  => _x('Clases', 'Post Type General Name', 'extremegymfitness'),
+        'singular_name'         => _x('Clase', 'Post Type Singular Name', 'extremegymfitness'),
+        'menu_name'             => __('Clases', 'extremegymfitness'),
+        'name_admin_bar'        => __('Clase', 'extremegymfitness'),
+        'archives'              => __('Archivo', 'extremegymfitness'),
+        'attributes'            => __('Atributos', 'extremegymfitness'),
+        'parent_item_colon'     => __('Clase Padre', 'extremegymfitness'),
+        'all_items'             => __('Todas Las Clases', 'extremegymfitness'),
+        'add_new_item'          => __('Agregar Clase', 'extremegymfitness'),
+        'add_new'               => __('Agregar Clase', 'extremegymfitness'),
+        'new_item'              => __('Nueva Clase', 'extremegymfitness'),
+        'edit_item'             => __('Editar Clase', 'extremegymfitness'),
+        'update_item'           => __('Actualizar Clase', 'extremegymfitness'),
+        'view_item'             => __('Ver Clase', 'extremegymfitness'),
+        'view_items'            => __('Ver Clases', 'extremegymfitness'),
+        'search_items'          => __('Buscar Clase', 'extremegymfitness'),
+        'not_found'             => __('No Encontrado', 'extremegymfitness'),
+        'not_found_in_trash'    => __('No Encontrado en Papelera', 'extremegymfitness'),
+        'featured_image'        => __('Imagen Destacada', 'extremegymfitness'),
+        'set_featured_image'    => __('Guardar Imagen destacada', 'extremegymfitness'),
+        'remove_featured_image' => __('Eliminar Imagen destacada', 'extremegymfitness'),
+        'use_featured_image'    => __('Utilizar como Imagen Destacada', 'extremegymfitness'),
+        'insert_into_item'      => __('Insertar en Clase', 'extremegymfitness'),
+        'uploaded_to_this_item' => __('Agregado en Clase', 'extremegymfitness'),
+        'items_list'            => __('Lista de Clases', 'extremegymfitness'),
+        'items_list_navigation' => __('Navegación de Clases', 'extremegymfitness'),
+        'filter_items_list'     => __('Filtrar Clases', 'extremegymfitness'),
     );
     $args = array(
-        'label'                 => __('Clase', 'gymfitness'),
-        'description'           => __('Clases para el Sitio Web', 'gymfitness'),
+        'label'                 => __('Clase', 'extremegymfitness'),
+        'description'           => __('Clases para el Sitio Web', 'extremegymfitness'),
         'labels'                => $labels,
         'supports'              => array('title', 'editor', 'thumbnail'),
         'hierarchical'          => true, // true = posts , false = paginas
@@ -63,6 +63,6 @@ function gymfitness_clases_post_type()
         'publicly_queryable'    => true,
         'capability_type'       => 'page',
     );
-    register_post_type('gymfitness_clases', $args);
+    register_post_type('extremegymfitness_clases', $args);
 }
-add_action('init', 'gymfitness_clases_post_type', 0);
+add_action('init', 'extremegymfitness_clases_post_type', 0);
